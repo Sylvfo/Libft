5c255bfeea3e478ea868ed526cb477802421afc3
sforster <syslive-creative-co@outlook.com>
2023-10-25 18:25:21 +0200

    ¨mercredisoir

--- ft_memset.c
+++ ft_memset.c
@@ -10,9 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include ¨libft.h¨
 
-int ft_memset(void *b, int c, size_t len)
+void *ft_memset(void *b, int c, size_t len)
 {
-	
+    unsigned char   *p;
+    size_t  i;
+
+    p = (unsigned char*)b;
+    i = 0;
+    while(i < len)
+    {
+        p[i] = c;
+        i++;
+    }
+    return (b);
 }
